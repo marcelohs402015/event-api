a16eefcc31f50677a3fdf1743eaca5ce94d2ed2f
Marcelo Hernandes <ID+marcelohs402015@users.noreply.github.com>
2026-01-13 18:00:38 -0300

    first commit

---
+++ src/main/java/com/mstech/api/domain/address/Address.java
@@ -0,0 +1,33 @@
+package com.mstech.api.domain.address;
+
+import com.mstech.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+
+}
---
+++ src/main/java/com/mstech/api/domain/coupon/Coupon.java
@@ -0,0 +1,33 @@
+package com.mstech.api.domain.coupon;
+
+import com.mstech.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
